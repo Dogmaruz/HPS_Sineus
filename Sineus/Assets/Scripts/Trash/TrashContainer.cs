13fe559a6ae9c386ac38a6f50d9a462dc1fd249a
Skvortsov-Mikhail <skw-misha@yandex.ru>
2023-09-23 12:20:03 +0300

    Обновил CityLevel (добавил модели зданий)

---
+++ Sineus/Assets/Scripts/Trash/TrashContainer.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashContainer : MonoBehaviour
+{
+
+    public List<Trash> Trashes;
+
+    private void Start()
+    {
+        Trashes = new List<Trash>();
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Trashes.Add(transform.GetChild(i).GetComponent<Trash>());
+        }
+    }
+
+    public bool HasTrashNear(Transform playerPosition, float detectionDistance)
+    {
+        foreach (var trash in Trashes)
+        {
+            float dist = Mathf.Abs(Vector3.Distance(trash.transform.position, playerPosition.position));
+
+            if (dist < detectionDistance) return true;
+        }
+
+        return false;
+    }
+}
